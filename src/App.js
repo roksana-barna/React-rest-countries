81cb46db4607ef9d4c312b11fb5aade150e62315
roksana-barna <roksanabarna1803@gmail.com>
2023-03-23 13:15:51 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < LoadCountries></LoadCountries>
+      
     </div>
   );
 }
+ function LoadCountries(){
+  const [countries,setCountries]=useState([]);
+  // opore array coz amra array k fetch korchi.
+  useEffect(()=>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then( res=>res.json())
+    .then(data=>setCountries(data))
+    
+
+  },[])
+  return(
+    <div>
+<h1>this this all the countries!!!!</h1>
+<h3>available:{countries.length}</h3>
+{
+countries.map(country=><Country name={country.name.common}></Country>)
+ }
+    </div>
+  )
+ }
+ function Country(props){
+  return(
+    <div>
+      <h2>Name:{props.name}</h2>
+    </div>
+  )
+ }
 
 export default App;
